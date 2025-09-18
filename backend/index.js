5ca8303e109cf9e06666b04b781a8d9bb9fdc9d5
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2025-09-19 00:52:32 +0530

    changes in file

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5001;
 
 connectDB();
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
+    origin:[
+        'http://localhost:5001',
+        "https://serene-cat-642896.netlify.app"
+    ],
     credentials:true
 }))
 app.use(express.json());
