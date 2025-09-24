062c9f10b00b46129031d691c03efd71810751ee
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2025-09-24 19:48:58 +0530

    add links

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -56,7 +56,7 @@ function Signup() {
                 </div>
                 <div className="right-part">
                     <h1>Signup</h1>
-                        {/* <p>Already have an account?<Link to={'https://quiz-website-rgfk.onrender.com/login'}>Login</Link></p> */}
+                        <p>Already have an account?<small onClick={()=>{navigate('https://quiz-website-rgfk.onrender.com/login')}}>Login</small></p>
                 </div>
             </div>
         </div>
